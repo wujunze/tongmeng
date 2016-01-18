d4dccc7a6ae66ae35ac5b18f3ebf706c715133e0
wujunze <itwujunze@163.com>
2016-01-18 20:41:44 +0800

    modefied

--- App/Home/Common/function.php
+++ App/Home/Common/function.php
@@ -1,12 +1,12 @@
 <?php
-//打印数组函数
+
      function p($array){
      dump($array,1,'<pre>',0);
    }
 
 
 
-//实现无限极分类
+
  function unLimitedForLayer($cate,$name='child',$pid=0){
 	$arr=array();
 	foreach($cate as $v){
